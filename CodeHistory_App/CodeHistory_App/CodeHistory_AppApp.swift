add3f40ae2d76cf5e7e2ae53a4390182604668ab
codes-by-pinewood <52709218+codes-by-pinewood@users.noreply.github.com>
2022-07-01 23:17:53 +0200

    Add files via upload

---
+++ CodeHistory_App/CodeHistory_App/CodeHistory_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistory_AppApp.swift
+//  CodeHistory_App
+//
+//  Created by Apple on 01/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistory_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
